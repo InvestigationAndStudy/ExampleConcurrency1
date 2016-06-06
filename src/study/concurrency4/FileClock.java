f8b0908db772d6b180feb9d505867aff40d2a20e
venya <mymyhighlight@gmail.com>
2016-06-06 20:07:41 +0300

    next example

--- src/study/concurrency4/FileClock.java
+++ src/study/concurrency4/FileClock.java
@@ -6,12 +6,13 @@ import java.util.concurrent.TimeUnit;
 public class FileClock implements Runnable {
 	@Override
 	public void run() {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 20; i++) {			
 			System.out.printf("%s\n", new Date());
 			try {
-				TimeUnit.SECONDS.sleep(1);
+				TimeUnit.SECONDS.sleep(1);				
 			} catch (InterruptedException e) {
 				System.out.printf("The FileClock has been interrupted");
+				Thread.currentThread().interrupt();
 			}
 		}
 	}
