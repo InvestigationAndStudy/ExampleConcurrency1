d8098d6de8a00e79b4594cb84e86117e490d2fdb
venya <mymyhighlight@gmail.com>
2016-06-09 20:45:00 +0300

    Added new couple of examples

---
+++ src/study/concurrency7/Main.java
@@ -0,0 +1,10 @@
+package study.concurrency7;
+
+public class Main {
+	public static void main(String[] args) {
+		Task task = new Task();
+		Thread thread = new Thread(task);
+		thread.setUncaughtExceptionHandler(new ExceptionHandler());
+		thread.start();
+	}
+}
---
+++ src/study/concurrency7/Task.java
@@ -0,0 +1,8 @@
+package study.concurrency7;
+
+public class Task implements Runnable {
+	@Override
+	public void run() {
+		int numero = Integer.parseInt("TTT");
+	}
+}
