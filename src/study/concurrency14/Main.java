3deffc6fb6ebefdc08638c65212adc136112ac0b
venya <mymyhighlight@gmail.com>
2016-11-19 14:02:12 +0200

    New examples

---
+++ src/study/concurrency14/Main.java
@@ -0,0 +1,16 @@
+package study.concurrency14;
+
+public class Main {
+	public static void main(String[] args) {
+		EventStorage storage=new EventStorage();
+		
+		Producer producer=new Producer(storage);
+		Thread thread1=new Thread(producer);
+		
+		Consumer consumer=new Consumer(storage);
+		Thread thread2=new Thread(consumer);
+		
+		thread2.start();
+		thread1.start();
+	}
+}
